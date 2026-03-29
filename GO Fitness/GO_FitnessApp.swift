2bb23a734a0730229703be78fe70b4bea7e042e9
Youssif Hany <youssifhany962@gmail.com>
2026-03-29 21:27:22 +0200

    Init commit

---
+++ GO Fitness/GO_FitnessApp.swift
@@ -0,0 +1,17 @@
+//
+//  GO_FitnessApp.swift
+//  GO Fitness
+//
+//  Created by Youssif Hany on 29/03/2026.
+//
+
+import SwiftUI
+
+@main
+struct GO_FitnessApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
